68c1c08959cb149488eee3de0ef400fa421ed176
thebladehit <boqdanyarmolka@gmail.com>
2024-05-14 23:54:01 +0300

    feature: add approve user logic
    add get not approved users
    add approve user
    add approve user dto

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersRepository } from '../repositories/users.repository';
+import { MailModule } from '../mail/mail.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [MailModule, PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}

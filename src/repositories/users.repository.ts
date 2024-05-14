b40b0f1c9d6b75a352cbf0de2f933ed1caf8b1fe
thebladehit <boqdanyarmolka@gmail.com>
2024-05-14 15:47:37 +0300

    feature: implement register and login
    add user repository
    add new dependencies
    add migration
    add add user entity
    add user module

---
+++ src/repositories/users.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { RegisterDto } from "../auth/dto/register.dto";
+import { User } from "@prisma/client";
+
+@Injectable()
+export class UsersRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email }
+    });
+  }
+
+  create(dto: RegisterDto) {
+    return this.prisma.user.create({
+      data: dto
+    });
+  }
+
+  update(id: string, data: Partial<User>) {
+    return this.prisma.user.update({
+      where: { id },
+      data: data
+    });
+  }
+}

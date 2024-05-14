716410e23eaaf301964c56822ad27197c0efa3ee
thebladehit <boqdanyarmolka@gmail.com>
2024-05-14 14:29:32 +0300

    feature: add basic auth module
    add register dto
    add login dto
    add decorator for validation of passwords equals

---
+++ src/utils/decorators/validate-passwords-equals.decorator.ts
@@ -0,0 +1,14 @@
+import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+import { RegisterDto } from "../../auth/dto/register.dto";
+
+@ValidatorConstraint({ name: 'IsPasswordEqual', async: false })
+export class IsEqualPasswordConstraint implements ValidatorConstraintInterface {
+  validate(passwordRepeat: string, validationArguments: ValidationArguments): boolean {
+    const obj = validationArguments.object as RegisterDto;
+    return obj.password === passwordRepeat;
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return 'Passwords are not the same';
+  }
+}

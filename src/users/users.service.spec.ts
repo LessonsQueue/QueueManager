cbff642d7d3c18869558e95dcfb64f109bb40212
SharpDevOps10 <td090833@gmail.com>
2024-05-20 17:14:04 +0300

    testing: added basic integration tests setup + several tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from '../repositories/users.repository';
+import { MailModule } from '../mail/mail.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, UsersRepository],
+      imports: [MailModule, PrismaModule, AuthModule],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
